972edd09771498d611a610e9c9ca10526eb542e8
Zinu <Zinu>
2017-05-10 13:02:01 -0700

    html skeleton & clckhandler with ajax skeleton

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 /**
  * Created by C4.17 Team 4 Hackathon 2 on 5/10/2017.
  */
+$(document).ready(function(){
+    clickHandler();
+});
+function clickHandler(){
+    $('button').click(function(){
+        console.log('click initiated');
+        ajaxCall();
+    });
+}
+
+function ajaxCall(){
+    $.ajax({
+        dataType: 'json',
+        url: ' ',
+
+        success: function(result){
+            console.log('success:' ,result);
+        },
+        error: function (result) {
+            console.log('error ', result);
+        }
+    });
+    console.log('');
+}

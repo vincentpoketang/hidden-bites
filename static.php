580bc8bd28449aa762efe253ff4195acbf4e6778
TJ Kinion <tjkinion@gmail.com>
2017-05-10 17:01:22 -0700

    more json

--- static.php
+++ static.php
@@ -1,5 +1,4 @@
 
-#!/usr/bin/php
 <?php
 /**
  * Yelp Fusion API code sample.
@@ -19,10 +18,8 @@
 // You can find them on
 // https://www.yelp.com/developers/v3/manage_app
 
-//require_once ('credentials.php');
+require_once ('credentials.php');
 
-$CLIENT_ID = 'WU6z7FsEd1hqqujqFUQXJw';
-$CLIENT_SECRET = 'nx1JYHE4BdrUA59YeLiEM219a7q6by7gBNcwE8fez9NiJ9hX6ldFDlTNX39LUogS';
 // Complain if credentials haven't been filled out.
 assert($CLIENT_ID, "Please supply your client_id.");
 assert($CLIENT_SECRET, "Please supply your client_secret.");
@@ -33,57 +30,55 @@ $BUSINESS_PATH = "/v3/businesses/";  // Business ID will come after slash.
 $TOKEN_PATH = "/oauth2/token";
 $GRANT_TYPE = "client_credentials";
 // Defaults for our simple example.
-$DEFAULT_TERM = "dinner";
-$DEFAULT_LOCATION = "San Francisco, CA";
-$SEARCH_LIMIT = 3;
+$DEFAULT_TERM = '"hole in the wall" tacos';
+$DEFAULT_LOCATION = "irvine, ca";
+$SEARCH_LIMIT = 49;
 /**
  * Given a bearer token, send a GET request to the API.
  *
  * @return   OAuth bearer token, obtained using client_id and client_secret.
  */
-//function obtain_bearer_token() {
-//    try {
-//        # Using the built-in cURL library for easiest installation.
-//        # Extension library HttpRequest would also work here.
-//        $curl = curl_init();
-//        if (FALSE === $curl)
-//            throw new Exception('Failed to initialize');
-//        $postfields = "client_id=" . $GLOBALS['CLIENT_ID'] .
-//            "&client_secret=" . $GLOBALS['CLIENT_SECRET'] .
-//            "&grant_type=" . $GLOBALS['GRANT_TYPE'];
-//        curl_setopt_array($curl, array(
-//            CURLOPT_URL => $GLOBALS['API_HOST'] . $GLOBALS['TOKEN_PATH'],
-//            CURLOPT_RETURNTRANSFER => true,  // Capture response.
-//            CURLOPT_ENCODING => "",  // Accept gzip/deflate/whatever.
-//            CURLOPT_MAXREDIRS => 10,
-//            CURLOPT_TIMEOUT => 30,
-//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//            CURLOPT_CUSTOMREQUEST => "POST",
-//            CURLOPT_POSTFIELDS => $postfields,
-//            CURLOPT_HTTPHEADER => array(
-//                "cache-control: no-cache",
-//                "content-type: application/x-www-form-urlencoded",
-//            ),
-//        ));
-//        $response = curl_exec($curl);
-//        if (FALSE === $response)
-//            throw new Exception(curl_error($curl), curl_errno($curl));
-//        $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-//        if (200 != $http_status)
-//            throw new Exception($response, $http_status);
-//        curl_close($curl);
-//    } catch(Exception $e) {
-//        trigger_error(sprintf(
-//            'Curl failed with error #%d: %s',
-//            $e->getCode(), $e->getMessage()),
-//            E_USER_ERROR);
-//    }
-//    $body = json_decode($response);
-//    $bearer_token = $body->access_token;
-//    return $bearer_token;
-//}
-
-$bearer_token = 'XRDgBGSKPf76v5v45-KWd52i3HjGdm1BtssOdMGtSiCgoKY46Ts-Y_-WsIFWDZthHw_dVlna99-tGQqzZOkmaUPbTf2wTk8SbOINUODGmJDbEimQKIMoWjcw9lITWXYx';
+function obtain_bearer_token() {
+    try {
+        # Using the built-in cURL library for easiest installation.
+        # Extension library HttpRequest would also work here.
+        $curl = curl_init();
+        if (FALSE === $curl)
+            throw new Exception('Failed to initialize');
+        $postfields = "client_id=" . $GLOBALS['CLIENT_ID'] .
+            "&client_secret=" . $GLOBALS['CLIENT_SECRET'] .
+            "&grant_type=" . $GLOBALS['GRANT_TYPE'];
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $GLOBALS['API_HOST'] . $GLOBALS['TOKEN_PATH'],
+            CURLOPT_RETURNTRANSFER => true,  // Capture response.
+            CURLOPT_ENCODING => "",  // Accept gzip/deflate/whatever.
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => $postfields,
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "content-type: application/x-www-form-urlencoded",
+            ),
+        ));
+        $response = curl_exec($curl);
+        if (FALSE === $response)
+            throw new Exception(curl_error($curl), curl_errno($curl));
+        $http_status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if (200 != $http_status)
+            throw new Exception($response, $http_status);
+        curl_close($curl);
+    } catch(Exception $e) {
+        trigger_error(sprintf(
+            'Curl failed with error #%d: %s',
+            $e->getCode(), $e->getMessage()),
+            E_USER_ERROR);
+    }
+    $body = json_decode($response);
+    $bearer_token = $body->access_token;
+    return $bearer_token;
+}
 /**
  * Makes a request to the Yelp API and returns the response
  *
@@ -166,20 +161,24 @@ function get_business($bearer_token, $business_id) {
 function query_api($term, $location) {
     $bearer_token = obtain_bearer_token();
     $response = json_decode(search($bearer_token, $term, $location));
+    for($i=0; $i < 4; $i++){
+        $pretty_response = json_encode($response->businesses[$i], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        print_r($pretty_response);
+    }
     $business_id = $response->businesses[0]->id;
-
-    print sprintf(
-        "%d businesses found, querying business info for the top result \"%s\"\n\n",
-        count($response->businesses),
-        $business_id
-    );
+//    print sprintf(
+//        "%d businesses found, querying business info for the top result \"%s\"\n\n",
+//        count($response->businesses),
+//        $business_id
+//    );
 
     $response = get_business($bearer_token, $business_id);
 
-    print sprintf("Result for business \"%s\" found:\n", $business_id);
+   //print sprintf("Result for business \"%s\" found:\n", $business_id);
     $pretty_response = json_encode(json_decode($response), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
     print "$pretty_response\n";
 }
+
 /**
  * User input is handled here
  */
@@ -193,5 +192,3 @@ $term = $options['term'] ?: $GLOBALS['DEFAULT_TERM'];
 $location = $options['location'] ?: $GLOBALS['DEFAULT_LOCATION'];
 query_api($term, $location);
 ?>
-Contact GitHub API Training Shop Blog About
-© 2017 GitHub, Inc. Terms Privacy Security Status Help

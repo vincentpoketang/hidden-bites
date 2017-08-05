cf0daa78333fa463eb3b46f72beb1fc2efb5d33d
Vincent Tang <vincet3@uci.edu>
2017-08-05 14:50:30 -0700

    more minor redirect

--- client/js/marker-clusterer.js
+++ client/js/marker-clusterer.js
@@ -186,7 +186,7 @@ function MarkerClusterer(map, opt_markers, opt_options) {
  * @type {string}
  * @private
  */
-MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = '../img/m';
+MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = 'client/img/m';
 
 
 /**
